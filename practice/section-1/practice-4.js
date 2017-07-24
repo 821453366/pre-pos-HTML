5bec0ecc87b31f31f47c85addb5f6d59331fd8bd
821453366 <821453366@qq.com>
2017-07-24 12:50:57 +0000

    第四题：选出A集合中元素的key属性，跟B对象中value属性中的元素相同的元素

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+   var collection_same = [];
+   
+  for (var i = 0; i < collectionA.length; i++) {
+    if (isExistCollectionAIncludeObjectB((collectionA[i]).key, objectB.value)) {
+      collection_same.push((collectionA[i]).key);
+    }
+  }
+
+  return collection_same;
+}
+
+function isExistCollectionAIncludeObjectB(collectionA, objectB) {
+  for (var index in objectB) {
+    if (objectB[index] === collectionA)
+
+      return true;
+  }
+  
+  return false;
 }
