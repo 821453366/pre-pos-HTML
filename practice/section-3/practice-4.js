cf482be26d3e9f81905ddc3352f1db0c9db92526
821453366 <821453366@qq.com>
2017-07-24 15:03:02 +0000

    section-3：完成第四道题目

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,34 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var collection_same = [];
+  var key_value = "";
+  var count_value = 0;
+
+  for(var i = 0; i < collection.length;i++){
+    if(collection[i] != -1){
+      key_value = collection[i];
+      var keyCountObj = isExist( collection,key_value, count_value);
+      collection_same.push(keyCountObj);
+      count_value = 0;
+    }
+  }
+  for (var index in collection_same){
+    for(var index_b in object_b.value){
+      if(collection_same[index].key === object_b.value[index_b]){
+        collection_same[index].count-=Math.floor((collection_same[index].count)/3);
+      }
+    }
+  }
+  return collection_same;
 }
+
+function isExist(collection,key_value, count_value) {
+  for (var j = 0; j < collection.length; j++) {
+    if (key_value === collection[j]) {
+      count_value++;
+      collection[j] = -1;
+    }
+  }
+  var keyCountObj = {key: key_value, count: count_value};
+  return keyCountObj;
