6b33f0d11f8485c58a9640591878aaf49b41eeb0
821453366 <821453366@qq.com>
2017-07-24 12:34:28 +0000

    完成第一道题目

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var collection_same=[];
+  var q=0;
+  
+  for( var i = 0 ; i<collectionA.length;i++){
+    for(var j = 0; j<collectionB.length;j++){
+      if(collectionA[i] === collectionB[j]){
+        collection_same[q++] =collectionB[j];
+      }
+    }
+  }
+  
+  return collection_same;
 }
