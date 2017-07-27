660bdd9492bd53085f0431b088b145d595946aa4
821453366 <821453366@qq.com>
2017-07-27 03:10:12 +0000

    任务二最后一道题

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,26 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let result = [];
+  let data =[];
+  let result_sum=[];
+
+  collection.forEach((element, index) => {
+    if (element.length > 1) {
+      let temporaryArray = element.split("");
+      let number = parseInt(element.replace(/[^0-9]/ig, ""));
+      for(let i = 0;i<number-1;i++){
+        result.push(temporaryArray[0])
+      }
+      collection.splice(index, 1,temporaryArray[0]);
+      result_sum = collection.concat(result);
+
+    }
+  });
+  result_sum.forEach(item => {
+    if (!data.find(element => element.name === item))
+      data.push({name: item, summary: result_sum.filter(element => element === item).length});
+  });
+
+  return data;
 }
