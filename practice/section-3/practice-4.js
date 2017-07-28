3d25320547582ee695ae6335a54aee5f70ff2938
821453366 <821453366@qq.com>
2017-07-28 08:59:29 +0000

    完成

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,29 +1,40 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  var collection_same = [];
+   var collection_same = [];
   var key_value = "";
   var count_value = 0;
+  var arr = [];
 
-  for(var i = 0; i < collection.length;i++){
-    if(collection[i] != -1){
-      key_value = collection[i];
-      var keyCountObj = isExist( collection,key_value, count_value);
-      collection_same.push(keyCountObj);
-      count_value = 0;
+  for (var i = 0; i < collectionA.length; i++) {
+    if (collectionA[i] != -1) {
+      if (collectionA[i].indexOf("-") > -1) {
+        arr = collectionA[i].split("-");
+        key_value = arr[0];
+        count_value = parseInt(arr[1]);
+        var keyCountObj = {key: key_value, count: count_value};
+        collection_same.push(keyCountObj);
+        count_value = 0;
+      } else {
+        key_value = collectionA[i];
+        var keyCountObj = isExist(collectionA, key_value, count_value);
+        collection_same.push(keyCountObj);
+        count_value = 0;
+      }
     }
   }
   for (var index in collection_same){
-    for(var index_b in object_b.value){
-      if(collection_same[index].key === object_b.value[index_b]){
+    for(var index_b in objectB.value){
+      if(collection_same[index].key === objectB.value[index_b]){
         collection_same[index].count-=Math.floor((collection_same[index].count)/3);
       }
     }
   }
+
+
   return collection_same;
 }
-
-function isExist(collection,key_value, count_value) {
+function isExist(collection, key_value, count_value) {
   for (var j = 0; j < collection.length; j++) {
     if (key_value === collection[j]) {
       count_value++;
@@ -31,4 +42,6 @@ function isExist(collection,key_value, count_value) {
     }
   }
   var keyCountObj = {key: key_value, count: count_value};
+
   return keyCountObj;
+}
